a1d544850051b30a7dee30402a27a9db354c5e28
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:34:11 +1000

    Create Q1_Practice.py

---
+++ 22T3_哈马迪/Q1_Practice.py
@@ -0,0 +1,33 @@
+# COMP9021 22T3 - Rachid Hamadi
+# Final Exam Question 1
+
+def count_of_given_digit_in_numbers_in_list(L, d):
+    '''
+    You can assume that L is a list of positive integers and d is a digit.
+    
+    >>> count_of_given_digit_in_numbers_in_list([3, 3333, 33], 3)
+    7
+    >>> count_of_given_digit_in_numbers_in_list([13, 14, 258], 9)
+    0
+    >>> count_of_given_digit_in_numbers_in_list([1, 21, 30, 1411], 1)
+    5
+    >>> count_of_given_digit_in_numbers_in_list([4082, 5395, 7888, 5697], 0)
+    1
+    >>> count_of_given_digit_in_numbers_in_list([4082, 5395, 7888, 5697], 1)
+    0
+    >>> count_of_given_digit_in_numbers_in_list([4082, 5395, 7888, 5697], 7)
+    2
+    >>> count_of_given_digit_in_numbers_in_list([4082, 5395, 7888, 5697], 8)
+    4
+    '''
+
+    # REPLACE return WITH YOUR CODE
+    count = 0
+    for number in L:
+        count += str(number).count(str(d))
+    return count
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
