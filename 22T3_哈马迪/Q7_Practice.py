87dcc3143d6e937c59cf568cd4692510465bf20f
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:33:53 +1000

    Create Q7_Practice.py

---
+++ 22T3_哈马迪/Q7_Practice.py
@@ -0,0 +1,137 @@
+# COMP9021 22T3 - Rachid Hamadi
+# Final Exam Question 7
+
+from random import seed, randrange
+
+
+def area(for_seed, sparsity, i, j):
+    '''
+    You can assume that i and j are both between 0 and 9 included.
+    i is the row number (indexed from top to bottom),
+    j is the column number (indexed from left to right)
+    of the displayed grid.
+
+    >>> area(0, 1, 5, 5)
+    The grid is:
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 1 1 1 1
+    The area of the largest empty region of the grid
+    containing the point (5, 5) is: 0
+    >>> area(0, 1000, 5, 5)
+    The grid is:
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0
+    The area of the largest empty region of the grid
+    containing the point (5, 5) is: 100
+    >>> area(0, 3, 6, 2)
+    The grid is:
+    0 0 1 0 0 0 0 0 0 0
+    0 1 0 1 0 1 1 0 0 0
+    0 0 1 0 1 0 1 0 0 0
+    0 1 0 0 0 0 0 1 0 0
+    0 0 0 1 0 1 1 0 0 0
+    0 0 1 0 0 0 1 0 0 0
+    1 1 0 1 1 1 0 0 1 1
+    0 0 0 1 0 0 0 0 1 0
+    0 0 1 0 0 0 0 0 1 0
+    0 0 0 1 0 1 1 1 1 0
+    The area of the largest empty region of the grid
+    containing the point (6, 2) is: 9
+    >>> area(0, 2, 9, 5)
+    The grid is:
+    0 0 1 0 0 0 0 0 0 1
+    1 0 1 1 0 1 0 1 1 0
+    0 1 0 0 0 1 0 0 0 1
+    1 1 0 1 0 0 1 0 1 1
+    1 1 1 0 1 1 0 0 1 0
+    0 1 0 1 0 0 1 0 0 1
+    0 1 1 1 1 0 0 1 1 1
+    1 1 1 0 0 1 1 0 0 0
+    0 0 1 0 1 0 0 1 1 1
+    0 1 1 0 1 0 0 1 1 1
+    The area of the largest empty region of the grid
+    containing the point (9, 5) is: 4
+    >>> area(0, 2, 2, 7)
+    The grid is:
+    0 0 1 0 0 0 0 0 0 1
+    1 0 1 1 0 1 0 1 1 0
+    0 1 0 0 0 1 0 0 0 1
+    1 1 0 1 0 0 1 0 1 1
+    1 1 1 0 1 1 0 0 1 0
+    0 1 0 1 0 0 1 0 0 1
+    0 1 1 1 1 0 0 1 1 1
+    1 1 1 0 0 1 1 0 0 0
+    0 0 1 0 1 0 0 1 1 1
+    0 1 1 0 1 0 0 1 1 1
+    The area of the largest empty region of the grid
+    containing the point (2, 7) is: 22
+    >>> area(0, 4, 2, 7)
+    The grid is:
+    0 0 1 0 0 0 0 0 0 0
+    0 0 0 1 0 0 0 1 1 0
+    0 1 0 0 0 0 0 0 0 1
+    1 1 0 1 0 0 0 0 1 0
+    0 0 0 0 1 1 0 0 1 0
+    0 1 0 0 0 0 1 0 0 0
+    0 0 0 0 1 0 0 1 1 0
+    0 1 1 0 0 0 0 0 0 0
+    0 0 1 0 1 0 0 0 0 1
+    0 1 0 0 0 0 0 1 1 0
+    The area of the largest empty region of the grid
+    containing the point (2, 7) is: 73
+    '''
+
+    seed(for_seed)
+    grid = [[int(randrange(sparsity) == 0) for _ in range(10)]
+            for _ in range(10)]
+    print('The grid is:')
+    for row in grid:
+        print(*row)
+    print('The area of the largest empty region of the grid')
+    print(f'containing the point ({i}, {j}) is: ', end='')
+
+    
+    # ADD YOUR CODE HERE (A PRINT STATEMENT AT THE END IS NEEDED)
+    visited = []
+    dfs(i,j,grid,visited)
+    print(len(visited))
+
+def dfs(i,j,grid,visited):
+    if grid[i][j] != 0 or (i,j) in visited:
+        return
+    visited.append((i,j))
+    neighbours = get_neighbour(i,j,grid)
+    for neighbour in neighbours:
+        dfs(neighbour[0],neighbour[1],grid,visited)
+
+def get_neighbour(i,j,grid):
+    directions = [(0,-1),(0,1),(-1,0),(1,0)]
+    neighbours = []
+    for direction in directions:
+        temp_i, temp_j = i + direction[0], j + direction[1]
+        if temp_i in range(10) and temp_j in range(10):
+            neighbours.append((temp_i,temp_j))
+    return neighbours
+
+    # POSSIBLY DEFINE OTHER FUNCTIONS
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
