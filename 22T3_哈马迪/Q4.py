391047194714f7648be6ee5c139a293a3d3fd1ba
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:34:04 +1000

    Create Q4.py

---
+++ 22T3_哈马迪/Q4.py
@@ -0,0 +1,41 @@
+# COMP9021 22T3 - Rachid Hamadi
+# Final Exam Question 4
+
+'''
+No point to hard code for small values of n, will be tested
+only for large enough values...
+'''
+
+
+def pascal_triangle_line(n):
+    '''
+    Recall: it is the list of binomial coefficients that give the
+    number of ways of choosing k objects out of n - 1 for 0 <= k < n.
+
+    >>> pascal_triangle_line(1)
+    [1]
+    >>> pascal_triangle_line(2)
+    [1, 1]
+    >>> pascal_triangle_line(3)
+    [1, 2, 1]
+    >>> pascal_triangle_line(4)
+    [1, 3, 3, 1]
+    >>> pascal_triangle_line(5)
+    [1, 4, 6, 4, 1]
+    >>> pascal_triangle_line(6)
+    [1, 5, 10, 10, 5, 1]
+    >>> pascal_triangle_line(7)
+    [1, 6, 15, 20, 15, 6, 1]
+    >>> pascal_triangle_line(8)
+    [1, 7, 21, 35, 35, 21, 7, 1]
+    '''
+
+    
+    return
+    # REPLACE return WITH YOUR CODE
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
