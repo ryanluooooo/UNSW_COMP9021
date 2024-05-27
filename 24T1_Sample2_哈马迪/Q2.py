a08a8df37944bb0ef39f51051fd6108020b0114b
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:30:36 +1000

    Create Q2.py

---
+++ 24T1_Sample2_哈马迪/Q2.py
@@ -0,0 +1,45 @@
+# COMP9021 24T1 - Rachid Hamadi
+# Sample Exam 2
+
+
+def f(N):
+    '''
+    >>> f(20)
+    Here are your banknotes:
+    $20: 1
+    >>> f(40)
+    Here are your banknotes:
+    $20: 2
+    >>> f(42)
+    Here are your banknotes:
+    $2: 1
+    $20: 2
+    >>> f(43)
+    Here are your banknotes:
+    $1: 1
+    $2: 1
+    $20: 2
+    >>> f(45)
+    Here are your banknotes:
+    $5: 1
+    $20: 2
+    >>> f(2537)
+    Here are your banknotes:
+    $2: 1
+    $5: 1
+    $10: 1
+    $20: 1
+    $100: 25
+    '''
+    banknote_values = [1, 2, 5, 10, 20, 50, 100]
+    banknotes = dict.fromkeys(banknote_values, 0)
+    # Insert your code here
+    print('Here are your banknotes:')
+    for value in sorted(banknotes):
+        if banknotes[value]:
+            print('${}: {}'.format(value, banknotes[value]))
+                        
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
