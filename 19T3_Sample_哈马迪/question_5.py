8d174394952a9452810c5317ce0e9ee03e5f7f3d
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:35:09 +1000

    Create question_5.py

---
+++ 19T3_Sample_哈马迪/question_5.py
@@ -0,0 +1,40 @@
+# COMP9021 19T3 - Rachid Hamadi
+# Sample Exam Question 5
+
+
+'''
+Will be tested with year between 1913 and 2013.
+You might find the reader() function of the csv module useful,
+but you can also use the split() method of the str class.
+'''
+
+import csv
+
+def f(year):
+    '''
+    >>> f(1914)
+    In 1914, maximum inflation was: 2.0
+    It was achieved in the following months: Aug
+    >>> f(1922)
+    In 1922, maximum inflation was: 0.6
+    It was achieved in the following months: Jul, Oct, Nov, Dec
+    >>> f(1995)
+    In 1995, maximum inflation was: 0.4
+    It was achieved in the following months: Jan, Feb
+    >>> f(2013)
+    In 2013, maximum inflation was: 0.82
+    It was achieved in the following months: Feb
+    '''
+    months = 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    # Insert your code here
+    
+                    
+    print(f'In {year}, maximum inflation was: {max_inf}')
+    print(f'It was achieved in the following months: {", ".join(L)}')                    
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
