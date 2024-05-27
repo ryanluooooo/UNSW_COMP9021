48ddadb0b0fbbb765764f88243d56eccfe22b418
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:43:29 +1000

    Create q8.py

---
+++ 19T2/q8.py
@@ -0,0 +1,35 @@
+
+dictionary_file = 'dictionary.txt'
+
+def number_of_words_in_dictionary(word_1, word_2):
+    '''
+    "dictionary.txt" is stored in the working directory.
+
+    >>> number_of_words_in_dictionary('company', 'company')
+    Could not find company in dictionary.
+    >>> number_of_words_in_dictionary('company', 'comparison')
+    Could not find at least one of company and comparison in dictionary.
+    >>> number_of_words_in_dictionary('COMPANY', 'comparison')
+    Could not find at least one of COMPANY and comparison in dictionary.
+    >>> number_of_words_in_dictionary('company', 'COMPARISON')
+    Could not find at least one of company and COMPARISON in dictionary.
+    >>> number_of_words_in_dictionary('COMPANY', 'COMPANY')
+    COMPANY is in dictionary.
+    >>> number_of_words_in_dictionary('COMPARISON', 'COMPARISON')
+    COMPARISON is in dictionary.
+    >>> number_of_words_in_dictionary('COMPANY', 'COMPARISON')
+    Found 14 words between COMPANY and COMPARISON in dictionary.
+    >>> number_of_words_in_dictionary('COMPARISON', 'COMPANY')
+    Found 14 words between COMPARISON and COMPANY in dictionary.
+    >>> number_of_words_in_dictionary('CONSCIOUS', 'CONSCIOUSLY')
+    Found 2 words between CONSCIOUS and CONSCIOUSLY in dictionary.
+    >>> number_of_words_in_dictionary('CONSCIOUS', 'CONSCIENTIOUS')
+    Found 3 words between CONSCIOUS and CONSCIENTIOUS in dictionary.
+    '''
+    print()
+    # REPLACE THE PREVIOUS LINE WITH YOUR CODE
+
+               
+if __name__ == '__main__':     
+    import doctest
+    doctest.testmod()
