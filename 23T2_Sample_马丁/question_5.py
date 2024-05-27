7801365ffd8b2192c9dba06936f8e93d16dd95bb
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:32:50 +1000

    Create question_5.py

---
+++ 23T2_Sample_马丁/question_5.py
@@ -0,0 +1,29 @@
+# Will be tested with year between 1913 and 2013.
+
+
+import csv
+
+
+def f(year):
+    '''
+    >>> f(1914)
+    In 1914, maximum inflation was: 2.0
+    It was achieved in the following months: Aug
+    >>> f(1922)
+    In 1922, maximum inflation was: 0.6
+    It was achieved in the following months: Jul, Oct, Nov, Dec
+    >>> f(1995)
+    In 1995, maximum inflation was: 0.4
+    It was achieved in the following months: Jan, Feb
+    >>> f(2013)
+    In 2013, maximum inflation was: 0.82
+    It was achieved in the following months: Feb
+    '''
+    months = 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',\
+             'Sep', 'Oct', 'Nov', 'Dec'
+    # INSERT YOUR CODE HERE
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
