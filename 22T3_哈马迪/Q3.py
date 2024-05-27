e53f09f5956a56d77e848d5ebeda6bcf9b764b92
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:34:07 +1000

    Create Q3.py

---
+++ 22T3_哈马迪/Q3.py
@@ -0,0 +1,36 @@
+# COMP9021 22T3 - Rachid Hamadi
+# Final Exam Question 3
+
+'''
+You might find the ord() function useful.
+'''
+
+
+def longest_leftmost_sequence_of_consecutive_letters(word):
+    '''
+    You can assume that "word" is a string of nothing but lowercase letters.
+
+    >>> longest_leftmost_sequence_of_consecutive_letters('')
+    ''
+    >>> longest_leftmost_sequence_of_consecutive_letters('a')
+    'a'
+    >>> longest_leftmost_sequence_of_consecutive_letters('zuba')
+    'z'
+    >>> longest_leftmost_sequence_of_consecutive_letters('ab')
+    'ab'
+    >>> longest_leftmost_sequence_of_consecutive_letters('bcab')
+    'bc'
+    >>> longest_leftmost_sequence_of_consecutive_letters('aefbxyzcrsdt')
+    'xyz'
+    >>> longest_leftmost_sequence_of_consecutive_letters('efghuvwrstuvabcde')
+    'rstuv'
+    '''
+
+
+    return
+# REPLACE return WITH YOUR CODE
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
