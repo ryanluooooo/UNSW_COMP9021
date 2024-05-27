d29f4e7a91919cbe920b1d9b778301fa9d4d8843
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:35:03 +1000

    Create question_8.py

---
+++ 19T3_Sample_哈马迪/question_8.py
@@ -0,0 +1,66 @@
+# COMP9021 19T3 - Rachid Hamadi
+# Sample Exam Question 8
+from itertools import permutations
+
+'''
+Will be tested with letters a string of DISTINCT UPPERCASE letters only.
+'''
+
+
+def f(letters):
+    '''
+    >>> f('ABCDEFGH')
+    There is no solution
+    >>> f('GRIHWSNYP')
+    The pairs of words using all (distinct) letters in "GRIHWSNYP" are:
+    ('SPRING', 'WHY')
+    >>> f('ONESIX')
+    The pairs of words using all (distinct) letters in "ONESIX" are:
+    ('ION', 'SEX')
+    ('ONE', 'SIX')
+    >>> f('UTAROFSMN')
+    The pairs of words using all (distinct) letters in "UTAROFSMN" are:
+    ('AFT', 'MOURNS')
+    ('ANT', 'FORUMS')
+    ('ANTS', 'FORUM')
+    ('ARM', 'FOUNTS')
+    ('ARMS', 'FOUNT')
+    ('AUNT', 'FORMS')
+    ('AUNTS', 'FORM')
+    ('AUNTS', 'FROM')
+    ('FAN', 'TUMORS')
+    ('FANS', 'TUMOR')
+    ('FAR', 'MOUNTS')
+    ('FARM', 'SNOUT')
+    ('FARMS', 'UNTO')
+    ('FAST', 'MOURN')
+    ('FAT', 'MOURNS')
+    ('FATS', 'MOURN')
+    ('FAUN', 'STORM')
+    ('FAUN', 'STROM')
+    ('FAUST', 'MORN')
+    ('FAUST', 'NORM')
+    ('FOAM', 'TURNS')
+    ('FOAMS', 'RUNT')
+    ('FOAMS', 'TURN')
+    ('FORMAT', 'SUN')
+    ('FORUM', 'STAN')
+    ('FORUMS', 'NAT')
+    ('FORUMS', 'TAN')
+    ('FOUNT', 'MARS')
+    ('FOUNT', 'RAMS')
+    ('FOUNTS', 'RAM')
+    ('FUR', 'MATSON')
+    ('MASON', 'TURF')
+    ('MOANS', 'TURF')
+    '''
+    dictionary = 'dictionary.txt'
+    solutions = []
+    # Insert your code here
+    
+        
+                            
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
