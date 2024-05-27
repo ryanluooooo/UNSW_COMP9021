72a12cce7dfe9f8ed4d945e7570232c0a7c9c756
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:43:34 +1000

    Create q5.py

---
+++ 19T2/q5.py
@@ -0,0 +1,32 @@
+# ord(c) returns the encoding of character c.
+# chr(e) returns the character encoded by e.
+
+
+def rectangle(width, height):
+    '''
+    Displays a rectangle by outputting lowercase letters, starting with a,
+    in a "snakelike" manner, from left to right, then from right to left,
+    then from left to right, then from right to left, wrapping around when z is reached.
+    
+    >>> rectangle(1, 1)
+    a
+    >>> rectangle(2, 3)
+    ab
+    dc
+    ef
+    >>> rectangle(3, 2)
+    abc
+    fed
+    >>> rectangle(17, 4)
+    abcdefghijklmnopq
+    hgfedcbazyxwvutsr
+    ijklmnopqrstuvwxy
+    ponmlkjihgfedcbaz
+    '''
+    print()
+    # REPLACE THE PREVIOUS LINE WITH YOUR CODE
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
