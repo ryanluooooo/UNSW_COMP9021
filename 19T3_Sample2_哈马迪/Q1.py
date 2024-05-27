161b2673c753adf89502290f389dcde943f173c4
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:35:34 +1000

    Create Q1.py

---
+++ 19T3_Sample2_哈马迪/Q1.py
@@ -0,0 +1,26 @@
+# COMP9021 19T3 - Rachid Hamadi
+# Sample Exam 2
+
+
+def remove_consecutive_duplicates(word):
+    '''
+    >>> remove_consecutive_duplicates('')
+    ''
+    >>> remove_consecutive_duplicates('a')
+    'a'
+    >>> remove_consecutive_duplicates('ab')
+    'ab'
+    >>> remove_consecutive_duplicates('aba')
+    'aba'
+    >>> remove_consecutive_duplicates('aaabbbbbaaa')
+    'aba'
+    >>> remove_consecutive_duplicates('abcaaabbbcccabc')
+    'abcabcabc'
+    >>> remove_consecutive_duplicates('aaabbbbbaaacaacdddd')
+    'abacacd'
+    '''
+    # Insert your code here (the output is returned, not printed out)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
