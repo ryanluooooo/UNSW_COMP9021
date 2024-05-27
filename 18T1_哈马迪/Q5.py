44c4e6c7a6ca64fdcb60dcc29e5995ef417882e6
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:35:47 +1000

    Create Q5.py

---
+++ 18T1_哈马迪/Q5.py
@@ -0,0 +1,26 @@
+def f(word):
+    '''
+    Recall that if c is an ascii character then ord(c) returns its ascii code.
+    Will be tested on nonempty strings of lowercase letters only.
+
+    >>> f('x')
+    The longest substring of consecutive letters has a length of 1.
+    The leftmost such substring is x.
+    >>> f('xy')
+    The longest substring of consecutive letters has a length of 2.
+    The leftmost such substring is xy.
+    >>> f('ababcuvwaba')
+    The longest substring of consecutive letters has a length of 3.
+    The leftmost such substring is abc.
+    >>> f('abbcedffghiefghiaaabbcdefgg')
+    The longest substring of consecutive letters has a length of 6.
+    The leftmost such substring is bcdefg.
+    >>> f('abcabccdefcdefghacdef')
+    The longest substring of consecutive letters has a length of 6.
+    The leftmost such substring is cdefgh.
+    '''
+    # Insert your code here
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
