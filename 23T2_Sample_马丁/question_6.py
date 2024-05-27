777324dec779e1850398afd13302c910fb0dece6
Ryan <150573650+ryanluooooo@users.noreply.github.com>
2024-05-27 17:32:49 +1000

    Create question_6.py

---
+++ 23T2_Sample_马丁/question_6.py
@@ -0,0 +1,66 @@
+# Will be tested with letters a string of DISTINCT UPPERCASE letters only.
+
+
+def f(letters):
+    '''
+    >>> f('ABCDEFGH')
+    There is no solution.
+    >>> f('GRIHWSNYP')
+    The pairs of words using all (distinct) letters in "GRIHWSNYP" are:
+    ('SPRING', 'WHY')
+    >>> f('ONESIX')
+    The pairs of words using all (distinct) letters in "ONESIX" are:
+    ('ION', 'SEX')
+    ('ONE', 'SIX')
+    >>> f('UTAROFSMN')
+    The pairs of words using all (distinct) letters in "UTAROFSMN" are:
+    ('AFT', 'MOURNS')
+    ('ANT', 'FORUMS')
+    ('ANTS', 'FORUM')
+    ('ARM', 'FOUNTS')
+    ('ARMS', 'FOUNT')
+    ('AUNT', 'FORMS')
+    ('AUNTS', 'FORM')
+    ('AUNTS', 'FROM')
+    ('FAN', 'TUMORS')
+    ('FANS', 'TUMOR')
+    ('FAR', 'MOUNTS')
+    ('FARM', 'SNOUT')
+    ('FARMS', 'UNTO')
+    ('FAST', 'MOURN')
+    ('FAT', 'MOURNS')
+    ('FATS', 'MOURN')
+    ('FAUN', 'STORM')
+    ('FAUN', 'STROM')
+    ('FAUST', 'MORN')
+    ('FAUST', 'NORM')
+    ('FOAM', 'TURNS')
+    ('FOAMS', 'RUNT')
+    ('FOAMS', 'TURN')
+    ('FORMAT', 'SUN')
+    ('FORUM', 'STAN')
+    ('FORUMS', 'NAT')
+    ('FORUMS', 'TAN')
+    ('FOUNT', 'MARS')
+    ('FOUNT', 'RAMS')
+    ('FOUNTS', 'RAM')
+    ('FUR', 'MATSON')
+    ('MASON', 'TURF')
+    ('MOANS', 'TURF')
+    '''
+    dictionary = 'dictionary.txt'
+    solutions = []
+    # INSERT YOUR CODE HERE
+    if not solutions:
+        print('There is no solution.')
+    else:
+        print(f'The pairs of words using all (distinct) letters '
+              f'in "{letters}" are:'
+             )
+        for solution in solutions:
+            print(solution)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
